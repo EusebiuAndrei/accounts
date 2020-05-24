6ea32260c7db3c9c23159810e8899ca53a621891
Georgiana <stativa50@gmail.com>
2020-05-24 18:08:18 +0300

    Provider Id

--- services/CourseService.js
+++ services/CourseService.js
@@ -36,7 +36,7 @@ class CourseService {
 					},
 				},
 			]);
-			course['userId'] = this.db.findOne({
+			course['userId'] = this.db.Provider.findOne({
 				_id: course.providerId,
 			});
 			console.log(course);
