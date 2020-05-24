5362807111143e08a52cc17416ed41bdd6195ba1
Georgiana <stativa50@gmail.com>
2020-05-24 18:36:30 +0300

    Provider Id

--- services/CourseService.js
+++ services/CourseService.js
@@ -36,9 +36,10 @@ class CourseService {
 					},
 				},
 			]);
-			course[0]['userId'] = await this.db.Provider.findOne({
-				_id: course[0]['providerId'],
-			}).userId;
+			course[0]['userId'] = '1234';
+			//await this.db.Provider.findOne({
+			// 	_id: course[0]['providerId'],
+			// }).userId;
 			if (course.length === 0) throw new Error('Not found');
 			return { success: true, data: course };
 		} catch (error) {
