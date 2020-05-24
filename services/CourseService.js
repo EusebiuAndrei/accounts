1c8236f238d3b8889ab8fb155188a10cad3a86a8
Georgiana <stativa50@gmail.com>
2020-05-24 18:54:12 +0300

    Provider Id

--- services/CourseService.js
+++ services/CourseService.js
@@ -36,9 +36,7 @@ class CourseService {
 					},
 				},
 			]);
-			course[0]['userId'] = await this.db.Provider.findOneById(
-				mongoose.Types.ObjectId(course[0].providerId),
-			);
+			course[0]['userId'] = course[0].providerId;
 			if (course.length === 0) throw new Error('Not found');
 			return { success: true, data: course };
 		} catch (error) {
